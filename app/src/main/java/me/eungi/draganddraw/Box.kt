59678d5ec048dde2d63b81e5c61fd5b7417f50f5
eungikim <eungikim.me@gmail.com>
2021-09-06 22:14:17 +0900

    Add feature for the rectangle to rotate

--- app/src/main/java/me/eungi/draganddraw/Box.kt
+++ app/src/main/java/me/eungi/draganddraw/Box.kt
@@ -5,7 +5,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-class Box(private val start: PointF, var end: PointF = start) : Parcelable {
+class Box(private val start: PointF, var end: PointF = start, var rotate: Float = 0f) : Parcelable {
 
     val left: Float
         get() = Math.min(start.x, end.x)
@@ -18,4 +18,11 @@ class Box(private val start: PointF, var end: PointF = start) : Parcelable {
 
     val bottom: Float
         get() = Math.max(start.y, end.y)
+
+    val center: PointF
+        get() {
+            val y = (bottom - top) / 2 + top
+            val x = (right - left) / 2 + left
+            return PointF(x, y)
+        }
 }
--- app/src/main/java/me/eungi/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/me/eungi/draganddraw/BoxDrawingView.kt
@@ -10,6 +10,8 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import kotlin.math.PI
+import kotlin.math.atan2
 
 private const val TAG = "BoxDrawingView"
 private const val KEY_VIEW = "KEY_VIEW"
@@ -18,6 +20,7 @@ private const val KEY_BOX_LIST = "KEY_BOX_LIST"
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
 
     private var currentBox: Box? = null
+    private var currentRotate: Float? = null
     private val boxen = mutableListOf<Box>()
     private val boxPaint = Paint().apply {
         color = 0x22ff0000.toInt()
@@ -31,39 +34,56 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(conte
         // 배경을 채운다
         canvas.drawPaint(backgroundPaint)
         boxen.forEach { box ->
+            canvas.save()
+            canvas.rotate(box.rotate, box.center.x, box.center.y)
             canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
+            canvas.restore()
         }
     }
 
 
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event ?: return super.onTouchEvent(event)
-        val current = PointF(event.x, event.y)
-        val action: String
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                action = "ACTION_DOWN"
-                // 그리기 상태를 재설정한다
-                currentBox = Box(current).also {
-                    boxen.add(it)
+//        printEvent(event)
+        val pointCount = event.pointerCount
+        for (p in 0 until pointCount) {
+            if (event.getPointerId(p) == 0) {
+                val current = PointF(event.getX(p), event.getY(p))
+                Log.d(TAG, "PointerId[0] action: ${event.action}")
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        // 박스 생성
+                        currentBox = Box(current).also {
+                            boxen.add(it)
+                        }
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        updateCurrentBox(current)
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        updateCurrentBox(current)
+                        currentBox = null
+                        currentRotate = null
+                    }
+                    MotionEvent.ACTION_CANCEL -> {
+                        currentBox = null
+                        currentRotate = null
+                    }
+                }
+            } else if (event.getPointerId(p) == 1) {
+                val current = PointF(event.getX(p), event.getY(p))
+                Log.d(TAG, "PointerId[1] action: ${event.action}")
+                when (event.action) {
+                    MotionEvent.ACTION_MOVE -> {
+                        rotateCurrentBox(current, currentRotate)
+                    }
+                    MotionEvent.ACTION_POINTER_2_UP -> {
+                        currentRotate = currentBox?.rotate
+                    }
                 }
             }
-            MotionEvent.ACTION_MOVE -> {
-                action = "ACTION_MOVE"
-                updateCurrentBox(current)
-            }
-            MotionEvent.ACTION_UP -> {
-                action = "ACTION_UP"
-                updateCurrentBox(current)
-                currentBox = null
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                action = "ACTION_CANCEL"
-                currentBox = null
-            }
-            else -> action = event.action.toString()
         }
-        Log.i(TAG, "$action at x=${current.x}, y=${current.y}")
         return true
     }
 
@@ -88,5 +108,46 @@ class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(conte
             invalidate()
         }
     }
+
+    private fun rotateCurrentBox(current: PointF, currentRotate: Float?) {
+        currentBox?.let {
+            val preRotate: Float = currentRotate ?: 0f
+            it.rotate = (calculationAngle(it.end to current) + preRotate) % 360
+            invalidate()
+        }
+    }
+
+    private fun calculationAngle(coordinates: Pair<PointF, PointF>): Float {
+        val x = coordinates.second.x - coordinates.first.x
+        val y = coordinates.second.y - coordinates.first.y
+        return (atan2(y, x) * 180 / PI).toFloat()
+    }
 }
 
+fun printEvent(ev: MotionEvent) {
+    val historySize = ev.historySize
+    val pointerCount = ev.pointerCount
+
+    val action = when(ev.action) {
+        0 -> "DOWN"
+        1 -> "UP"
+        2 -> "MOVE"
+        3 -> "CANCEL"
+        else -> "OTHER"
+    }
+
+    Log.d(TAG, "ME action $action pointer ${ev.actionIndex}")
+
+//    Log.d(TAG, "HistorySize:$historySize, pointerCount$pointerCount")
+//    for (h in 0 until historySize) {
+//        Log.d(TAG, "At time ${ev.getHistoricalEventTime(h)}:")
+//        for (p in 0 until pointerCount) {
+//            Log.d(TAG, "  pointer ${ev.getPointerId(p)}: (${ev.getHistoricalX(p, h)},${ev.getHistoricalY(p, h)})")
+//        }
+//    }
+//    System.out.printf("At time ${ev.eventTime}:")
+//    for (p in 0 until pointerCount) {
+//        System.out.printf(
+//            "  pointer ${ev.getPointerId(p)}: (${ev.getX(p)},${ev.getY(p)})")
+//    }
+}
